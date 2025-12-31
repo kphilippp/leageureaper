6ee43141f46b6c8d1f92956fb773488d3ce8e4d1
kphilippp <kevinphilip@gmail.com>
2025-12-31 13:45:00 -0600

    api working for different views; more information

--- espn_api/tester.py
+++ espn_api/tester.py
@@ -3,6 +3,8 @@ import os
 # since .env is in the root directory, we can load it like this
 from dotenv import load_dotenv
 load_dotenv()
+# for clean readablility
+import json
 
 league_id = os.getenv("LEAGUE_ID")
 team_id = os.getenv("TEAM_ID")
@@ -10,11 +12,38 @@ year = os.getenv("YEAR")
 espn_s2 = os.getenv("ESPN_S2")
 swid = os.getenv("SWID")
 
+
+# BASIC GET REQUEST TO FETCH LEAGUE DATA
+# url = f"https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{league_id}"
+# cookies = {
+#     "swid": swid,
+#     "espn_s2": espn_s2
+# }
+
+# response = requests.get(url, cookies=cookies)
+# print(json.dumps(response.json(), indent=4))
+
+# REQUEST WITH VIEWS
+views=[
+    "mMatchup",
+    "mMatchupScore",
+    "mTeam",
+    "mSettings",
+    "mStandings"
+    "mStatus",
+    "mRoster",
+    "mNav"
+]
 url = f"https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{league_id}"
+for view in views:
+    url += f"?view={view}"
+    if view != views[-1]:
+        url += "&"
 cookies = {
     "swid": swid,
     "espn_s2": espn_s2
 }
 
 response = requests.get(url, cookies=cookies)
-print(response.json())
+with open('league_data.json', 'w') as f:
+    f.write(json.dumps(response.json(), indent=4))
